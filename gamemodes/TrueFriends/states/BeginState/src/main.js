c41a2b42570b35faba980275c269df3afd2821ac
Ben Skinner <benjamin.skinner96@gmail.com>
2017-11-11 21:55:11 +1100

    Fixed ya prob bob

--- gamemodes/TrueFriends/states/BeginState/src/main.js
+++ gamemodes/TrueFriends/states/BeginState/src/main.js
@@ -2,6 +2,9 @@ exports.isValidated = function() {
     var readyClients = Object.keys(this.model.clientsReady).length;
     var numOfClients = GH.System.deviceManager.getAllDevicesOfType("client").length;
 
+    if(numOfClients < 2)
+        return false;
+
     console.log(readyClients + " : " + numOfClients);
     
     return readyClients >= numOfClients;
