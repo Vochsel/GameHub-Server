7f2cc6b32de9f0e47c3073ac230a5df83016147e
Ben Skinner <benjamin.skinner96@gmail.com>
2017-11-19 11:57:29 +1100

    Fixed ordering of results

--- gamemodes/TrueFriends/states/AnswerResults/src/main.js
+++ gamemodes/TrueFriends/states/AnswerResults/src/main.js
@@ -9,29 +9,29 @@ exports.isValidated = function () {
 }
 
 exports.onEnter = function () {
-    
-        //console.log(thisStage.model.clientSelections);
-        for (var idx in this.parentStage.model.clientSelections) {
-            var val = this.parentStage.model.clientSelections[idx];
-            //console.log(val);
-    
-            if (!Utils.Valid(this.parentStage.parentGM.model.clientScore[val.uid]))
-                this.parentStage.parentGM.model.clientScore[val.uid] = new Object();
-    
-            this.parentStage.parentGM.model.clientScore[val.uid].name = val.name;
-    
-            if (!Utils.Valid(this.parentStage.parentGM.model.clientScore[val.uid].totalVotes))
-                this.parentStage.parentGM.model.clientScore[val.uid].totalVotes = 0;
-    
-            this.parentStage.parentGM.model.clientScore[val.uid].totalVotes += val.selections;
-        }
 
-        //console.log(this.parentStage.parentGM.model);
-        console.log(this.parentStage.model.clientSelections)
-        this.parentStage.model.clientSelections = Object.keys(this.parentStage.model.clientSelections).map((k) => this.parentStage.model.clientSelections[k]);
-        console.log(this.parentStage.model.clientSelections)
-        this.parentStage.model.clientSelections.sort(function (a, b) {
-            return  b.selections - a.selections;
-        });
-    
+    //console.log(thisStage.model.clientSelections);
+    for (var idx in this.parentStage.model.clientSelections) {
+        var val = this.parentStage.model.clientSelections[idx];
+        //console.log(val);
+
+        if (!Utils.Valid(this.parentStage.parentGM.model.clientScore[val.uid]))
+            this.parentStage.parentGM.model.clientScore[val.uid] = new Object();
+
+        this.parentStage.parentGM.model.clientScore[val.uid].name = val.name;
+
+        if (!Utils.Valid(this.parentStage.parentGM.model.clientScore[val.uid].totalVotes))
+            this.parentStage.parentGM.model.clientScore[val.uid].totalVotes = 0;
+
+        this.parentStage.parentGM.model.clientScore[val.uid].totalVotes += val.selections;
     }
+
+    //console.log(this.parentStage.parentGM.model);
+    console.log(this.parentStage.model.clientSelections)
+    this.parentStage.model.clientSelections = Object.keys(this.parentStage.model.clientSelections).map((k) => this.parentStage.model.clientSelections[k]);
+    console.log(this.parentStage.model.clientSelections)
+    this.parentStage.model.clientSelections.sort(function (a, b) {
+        return b.selections - a.selections;
+    });
+
+}
