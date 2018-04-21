443dd3d7876a7bf086c533f05b8be73b137ef095
Ben Skinner <benjamin.skinner96@gmail.com>
2018-04-21 18:22:38 +1000

    Sort votes in WitsEnd and support WebKit animations

--- gamemodes/WitsEnd/states/gs_selections/review/src/events.js
+++ gamemodes/WitsEnd/states/gs_selections/review/src/events.js
@@ -3,6 +3,11 @@ exports.onEnter = function() {
     console.log(this.parentStage.parentGM.model.questions[this.parentStage.parentGM.model.currentAnswer]);
     this.model.currentQuestion = this.parentStage.parentGM.model.questions[this.parentStage.parentGM.model.currentAnswer];
 
+    this.model.currentQuestion.challengers = Array.from(this.model.currentQuestion.challengers);
+    this.model.currentQuestion.challengers.sort(function (a, b) {
+        return b.votes.length - a.votes.length;
+    });
+
     Utils.StartTimer(() => {
         GH.GMManager.Progress();
     }, 10);
